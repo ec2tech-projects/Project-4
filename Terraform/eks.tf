4bb6e25b7bf828372fdabb023ef43b2723ddb0b3
Akas Patranobis <akaspatranobis@yahoo.com>
2025-09-09 20:11:11 +0530

    up

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -34,7 +34,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "eks"
+  name     = "EKS_CLOUD"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
